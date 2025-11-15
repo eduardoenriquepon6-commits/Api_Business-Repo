24d67e893850d8db74c7b8acd09dc494a9069205
Eduardo Ponce <eduardoenriquepon6@gmail.com>
2025-11-14 21:37:32 -0600

    Añadiendo utils, conectando la DB y creando get_all_users

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
+from utils.database import execute_query_json
+import json
 
 app = FastAPI()
 
@@ -9,6 +10,20 @@ app = FastAPI()
 def read_root():
     return {"Hello": "Customer!"}
 
+@app.get("/users")
+async def get_all_users():
+    sqlscript = """
+        SELECT [id]
+            ,[nombre_completo]
+            ,[telefono]
+            ,[correo]
+            ,[fecha_registro]
+        FROM [ventas_directas].[usuarios]
+    """
+    result = await execute_query_json(sqlscript)
+    result_dict = json.loads(result)
+
+    return result_dict
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
