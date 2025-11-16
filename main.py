ae223919b02fed297114beb7f300a311208464e1
Eduardo Ponce <eduardoenriquepon6@gmail.com>
2025-11-16 13:39:18 -0600

    Implementando los endpoints de Stock

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from routes.usuarios import router as router_usuarios
 from routes.categoria import router as router_categoria
 from routes.productos import router as router_productos
+from routes.stock import router as router_stock
 
 app = FastAPI()
 
@@ -11,6 +12,8 @@ app.include_router(router_usuarios)
 app.include_router(router_categoria)
 
 app.include_router(router_productos)
+
+app.include_router(router_stock)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
---
+++ routes/stock.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, HTTPException, Request, status
+from models.stock import Stock
+from controllers.stock import (
+    get_all_stocks,
+    get_one_stck,
+    create_stock,
+    delete_stock,
+    update_stock
+)
+
+router = APIRouter(prefix = "/stock")
+
+@router.get( "/" , tags=["Stock"])
+async def get_products_stock():
+    result = await get_all_stocks()
+    return result
+
+@router.get("/{id}", tags=["Stock"])
+async def get_one_stock( id: int ):
+    result: Stock =  await get_one_stck(id)
+    return result
+
+@router.post( "/" , tags = ["Stock"])
+async def create_new_stock(stock_data: Stock):
+    result = await create_stock(stock_data)
+    return result
+
+@router.put("/{id}", tags=["Stock"])
+async def update_stock_information( stock_data: Stock , id: int ):
+    stock_data.id = id
+    result = await update_stock(stock_data)
+    return result
+
+@router.delete("/{id}", tags=["Stock"], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product_stock( id: int ):
+    status: str =  await delete_stock(id)
+    return status
