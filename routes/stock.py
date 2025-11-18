35f3c7c4bcc4dfd43813334436b56f0420800dc7
Eduardo Ponce <eduardoenriquepon6@gmail.com>
2025-11-18 17:48:30 -0600

    Realizando cambios finales, para subirlos a producción

--- routes/stock.py
+++ routes/stock.py
@@ -10,22 +10,22 @@ from controllers.stock import (
 
 router = APIRouter(prefix = "/stock")
 
-@router.get( "/" , tags=["Stock"])
+@router.get( "/" , tags=["Stock"], status_code=status.HTTP_200_OK)
 async def get_products_stock():
     result = await get_all_stocks()
     return result
 
-@router.get("/{id}", tags=["Stock"])
+@router.get("/{id}", tags=["Stock"], status_code=status.HTTP_200_OK)
 async def get_one_stock( id: int ):
     result: Stock =  await get_one_stck(id)
     return result
 
-@router.post( "/" , tags = ["Stock"])
+@router.post( "/" , tags = ["Stock"], status_code=status.HTTP_201_CREATED)
 async def create_new_stock(stock_data: Stock):
     result = await create_stock(stock_data)
     return result
 
-@router.put("/{id}", tags=["Stock"])
+@router.put("/{id}", tags=["Stock"], status_code=status.HTTP_200_OK)
 async def update_stock_information( stock_data: Stock , id: int ):
     stock_data.id = id
     result = await update_stock(stock_data)
--- routes/usuarios.py
+++ routes/usuarios.py
@@ -10,22 +10,22 @@ from controllers.usuarios import (
 
 router = APIRouter(prefix = "/usuarios")
 
-@router.get( "/" , tags=["Usuarios"])
+@router.get( "/" , tags=["Usuarios"], status_code=status.HTTP_200_OK)
 async def get_all_users():
     result = await get_all_u()
     return result
 
-@router.get("/{id}", tags=["Usuarios"])
+@router.get("/{id}", tags=["Usuarios"], status_code=status.HTTP_200_OK)
 async def get_one_user( id: int ):
     result: Usuarios =  await get_one_u(id)
     return result
 
-@router.post( "/" , tags = ["Usuarios"])
+@router.post( "/" , tags = ["Usuarios"], status_code=status.HTTP_201_CREATED)
 async def create_new_user(usuario_data: Usuarios):
     result = await create_user(usuario_data)
     return result
 
-@router.put("/{id}", tags=["Usuarios"])
+@router.put("/{id}", tags=["Usuarios"], status_code=status.HTTP_200_OK)
 async def update_user_information( usuario_data: Usuarios , id: int ):
     usuario_data.id = id
     result = await update_user(usuario_data)
